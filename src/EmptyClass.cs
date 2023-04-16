56d3da505b484531a0852a8668df076ee2e7a6c2
lamcnc <duclam289@gmail.com>
2023-04-16 16:18:55 +0700

    lam commit EmptyClass

---
+++ src/EmptyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Application
+{
+	public class EmptyClass
+	{
+		string name;
+		int age;
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+    }
+}
+
